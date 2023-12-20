64128b9e4a162cf11e6a9d4210ce3f83bf267171
Leonardo Paixão <leonardohspaixao@gmail.com>
2023-12-20 19:06:12 -0300

    (Comando login sempre cria novo user adm depois loga)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,23 +9,34 @@
 // ***********************************************
 //
 
+import createUserAdm from '../e2e/shared/createUserAdm';
+
 Cypress.Commands.add('login', () => {
-  cy.request({
-    log: true,
-    failOnStatusCode: true,
-    method: 'POST',
-    url: '/login',
-    headers: {
-      "accept": "application/json",
-      "content-type": "application/json"
-    },
-    body: {
-      "email": "fulano@qa.com",
-      "password": "teste"
-    },
+  // Cria o usuário primeiro
+  return createUserAdm().then((userData) => {
+    // Use os dados do usuário conforme necessário
+    const emailUsuario = userData.email;
+    const senhaUsuario = userData.password;
+
+    // Agora, faz a requisição para login
+    return cy.request({
+      failOnStatusCode: true,
+      method: 'POST',
+      url: '/login',
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json"
+      },
+      body: {
+        "email": emailUsuario,
+        "password": senhaUsuario
+      },
+    });
   }).then((response) => {
-    console.log(response);
+    localStorage.setItem('token', response.body.authorization);
+    expect(localStorage.getItem('token')).not.null;
     expect(response.status).to.equal(200);
     expect(response.body.message).to.equal("Login realizado com sucesso");
   });
-})
+});
+
