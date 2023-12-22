b317fbae48a3596171c015d43127fae80a84e94a
Leonardo Paixão <leonardohspaixao@gmail.com>
2023-12-22 15:44:42 -0300

    (Melhora o comando login)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,42 +1,67 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-
 import createUserAdm from '../e2e/shared/createUserAdm';
+import createUser from '../e2e/shared/createUser';
 
-Cypress.Commands.add('login', () => {
-  // Cria o usuário primeiro
-  return createUserAdm().then((userData) => {
-    // Use os dados do usuário conforme necessário
-    const emailUsuario = userData.email;
-    const senhaUsuario = userData.password;
+Cypress.Commands.add('login', (options = {}) => {
+  const { admin, normal } = options;
 
-    // Agora, faz a requisição para login
-    return cy.request({
-      failOnStatusCode: true,
-      method: 'POST',
-      url: '/login',
-      headers: {
-        "accept": "application/json",
-        "content-type": "application/json"
-      },
-      body: {
-        "email": emailUsuario,
-        "password": senhaUsuario
-      },
+  if (admin) {
+    return createUserAdm().then((userData) => {
+      // Use os dados do usuário conforme necessário
+      const emailUsuario = userData.email;
+      const senhaUsuario = userData.password;
+
+      // Agora, faz a requisição para login
+      return cy.request({
+        log: true,
+        failOnStatusCode: true,
+        method: 'POST',
+        url: '/login',
+        headers: {
+          "accept": "application/json",
+          "content-type": "application/json"
+        },
+        body: {
+          "email": emailUsuario,
+          "password": senhaUsuario
+        },
+      }).then((response) => {
+        console.log(response);
+        localStorage.setItem('token', response.body.authorization);
+        expect(localStorage.getItem('token')).not.null;
+        expect(response.status).to.equal(200);
+        expect(response.body.message).to.equal("Login realizado com sucesso");
+      });
     });
-  }).then((response) => {
-    localStorage.setItem('token', response.body.authorization);
-    expect(localStorage.getItem('token')).not.null;
-    expect(response.status).to.equal(200);
-    expect(response.body.message).to.equal("Login realizado com sucesso");
-  });
-});
 
+  } else if (normal) {
+    return createUser().then((userData) => {
+      const emailUsuario = userData.email;
+      const senhaUsuario = userData.password;
+
+      // Agora, faz a requisição para login
+      return cy.request({
+        log: true,
+        failOnStatusCode: true,
+        method: 'POST',
+        url: '/login',
+        headers: {
+          "accept": "application/json",
+          "content-type": "application/json"
+        },
+        body: {
+          "email": emailUsuario,
+          "password": senhaUsuario
+        },
+      }).then((response) => {
+        console.log(response)
+        localStorage.setItem('token', response.body.authorization);
+        expect(localStorage.getItem('token')).not.null;
+        expect(response.status).to.equal(200);
+        expect(response.body.message).to.equal("Login realizado com sucesso");
+      });
+    });
+  } else {
+    // Retorna uma promessa rejeitada com a mensagem de erro
+    return Promise.reject('Deve ser definido que tipo de usuário');
+  }
+});
