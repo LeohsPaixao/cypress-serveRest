b0ff61ad4e59f685da27d465b23998a465ed2f00
Leonardo Paixao <leonardohspaixao@gmail.com>
2024-11-20 11:55:16 -0300

    alguns ajustes

--- cypress/e2e/shared/apiRequest.js
+++ cypress/e2e/shared/apiRequest.js
@@ -1,3 +1,16 @@
+/**
+ * A function to make HTTP requests using Cypress' `cy.api` command.
+ * This function handles authentication and provides default headers.
+ *
+ * @param {Object} options - The options for the API request.
+ * @param {string} options.method - The HTTP method for the request.
+ * @param {string} options.url - The URL for the request.
+ * @param {Object} [options.body={}] - The request body. Default is an empty object.
+ * @param {boolean} [options.failOnStatusCode=true] - Whether to fail the test if the response status code is not successful. Default is true.
+ * @param {boolean} [options.auth=true] - Whether to include an Authorization header with the token from localStorage. Default is true.
+ *
+ * @returns {Chainable<Response>} - The Cypress chainable response object.
+ */
 export function apiRequest({ method, url, body = {}, failOnStatusCode = true, auth = true }) {
   const headers = {
     "accept": "application/json",
